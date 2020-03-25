676d6af3465219638edd03de5da2bc2e30841f76
Raghav Misra <raghav.m2014@gmail.com>
2020-03-25 13:49:21 -0700

    Bundle in ES5 custom elements adapter

--- src/mesh-ui.ts
+++ src/mesh-ui.ts
@@ -3,6 +3,7 @@ import { setHtmlProp } from './parts/set-property';
 import { render, displace } from './parts/dom';
 import { state } from './parts/state';
 import * as checkType from './parts/type-checks';
+import './parts/custom-elements-es5-adapter';
 
 export {
     jsx, html,
