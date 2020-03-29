bee8ba76999a219c2cc756efcf446282857db41a
Raghav Misra <raghav.m2014@gmail.com>
2020-03-29 09:57:41 -0700

    Refactor set property. Hopefully works

--- src/parts/set-property.ts
+++ src/parts/set-property.ts
@@ -6,37 +6,46 @@ const validProps = ["innerText", "value", "disabled", "classList", "nodeValue", 
 
 /* OP version of HTMLElement.setAttribute (supports events, special cases) */
 export function setHtmlProp(element: HTMLElement, name: string, value: any) {
-    const caselessName: string = name.toLowerCase().trim();
     const trimmedName: string = name.trim();
+    const caselessName: string = trimmedName.toLowerCase();
 
-    // If state, attach element:
-    if (isStateObject(value)) 
-        return value.attach(element, name);
+    // If state, call special state function:
+    if (isStateObject(value))
+        addPropIfState(element, trimmedName, value);
 
-    // If custom element watcher, attach to internal state:
+    // Individual style rules
+    if (name.startsWith("m-css:")) 
+        element.style[name.replace("m-css", "").trim()] = value.trim();
+
+    // Add event handlers:
+    if (name.startsWith("m-on:") && typeof value === "function")
+        element.addEventListener(name.replace("m-on:", "").trim(), value);
+
+    // If custom element watcher, call with internal state:
     else if (isAttributeWatcher(value)) 
-        return setHtmlProp(element, name, value.__meshInternalState__);
-
-    
-    else if (caselessName.startsWith("style::") && typeof value === "string")
-        element.style[caselessName.replace("style::", "")] = value;
-
-    // className becomes class:
-    else if (caselessName == "classname")
-        element.setAttribute("class", value);
-
-    // Event listeners as functions:
-    else if (caselessName.startsWith("on") && typeof value == "function")
-        element.addEventListener(caselessName.replace("on", ""), value);
+        addPropIfState(element, name, value.__meshInternalState__);
 
     // Property > Attributes:
     else if (validProps.indexOf(trimmedName) != -1)
         element[trimmedName] = value;
 
     // Apply as HTML attribute:
-    else element.setAttribute(name, value.toString());
+    else element.setAttribute(caselessName, value.toString());
 }
 
-export function styleFromObject(styleObject: Record<string, string>) {
+export function addPropIfState(element: HTMLElement, name: string, value: MeshUI.IStateValue<any>) {
+    const currentValue = value();
 
+    // Event handler exception:
+    if (name.startsWith("m-on:") && typeof currentValue === "function") {
+        const eventName = name.replace("m-on:", "").trim();
+        element.addEventListener(eventName, currentValue as EventListener);
+        value.attachCallback<Function>((oldValue, newValue) => {
+            element.removeEventListener(eventName, oldValue as EventListener);
+            element.addEventListener(eventName, newValue as EventListener);
+        }, null);
+    }
+
+    // Fallback, just attach value:
+    else value.attach(element, name);
 }
--- src/parts/type-checks.ts
+++ src/parts/type-checks.ts
@@ -1,9 +1,10 @@
-export function isStateObject(value) {
+/// <reference path="../../lib/mesh-ui.d.ts" />
+export function isStateObject(value): value is MeshUI.IStateValue {
     if (value.__isMeshStateFunction__ && typeof value === "function") return true;
     return false;
 }
 
-export function isAttributeWatcher(value) {
+export function isAttributeWatcher(value): value is MeshUI.IAttributeWatcher {
     if (value.__isMeshAttributeWatcher__ && typeof value === "function") return true;
     return false;
 }
